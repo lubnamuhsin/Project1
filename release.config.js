3430dc3573ef1b0dcfdf6b00858f65ba16007cc0
Nada Musthafa <93783722+NadaMusthafa@users.noreply.github.com>
2022-04-19 11:56:42 +0400

    fix: modified

--- release.config.js
+++ release.config.js
@@ -1,14 +1,14 @@
 module.exports = {
     extends: "semantic-release-monorepo",
     branches: "master",
-    repositoryUrl: "https://github.com/freelearn2000/Project1",
+    repositoryUrl: "https://github.com/NadaMusthafa/project1.git",
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
         ['@semantic-release/github', {
             assets: [
-                {path: "Node_build.zip", label: "Node Build"},
-                {path: "React_build.zip", label: "React Build"}
+                {path: "Node_build.zip", label: "Node_Build"},
+                {path: "React_build.zip", label: "React_Build"}
             ]
         }]
     ]
